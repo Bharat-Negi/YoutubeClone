31c65d2d806afce2ba9cb976b48e8e15210127a4
Bharat Singh Negi <bharat81183@gmail.com>
2024-09-13 11:13:19 +0530

    file up

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex justify-between px-4'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+    </div>
+  )
+}
+
+export default Navbar
